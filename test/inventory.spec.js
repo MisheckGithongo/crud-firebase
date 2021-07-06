545892d379e56722e39fd7faa4439a02f17c2c8e
misheckgithongo <misheckgithongo@gmail.com>
2021-07-07 00:19:24 +0300

    mocha instead of jest

--- test/inventory.spec.js
+++ test/inventory.spec.js
@@ -5,8 +5,6 @@ describe('adding a record', () => {
       const res = await request('localhost:5001').post('/crud-firebase-1166f/us-central1/addMessage?text=hello from test')
         .type('application/json')
         .send({
-        })
-      expect(res.statusCode).toEqual(200)
-      expect(res.body).toHaveProperty('result')
-    })
+        }).expect(200)
+    }).timeout(6000)
   })
