e262584d5b0d6cbbc35a29283461c0dcff50dc7f
misheckgithongo <misheckgithongo@gmail.com>
2021-07-07 11:54:54 +0300

    delete a record from inventory

--- test/inventory.spec.js
+++ test/inventory.spec.js
@@ -53,13 +53,26 @@ describe('POST /addRecord', () => {
     }).timeout(12000)
   })
 
-  // describe('DELETE /deleteRecord', () => {
-  //   it('should delete the record with the id from the inventory', async () => {
-  //     const res = await request('localhost:5001').put('/crud-firebase-1166f/us-central1/inventory/:1')
-  //       .type('application/json')
-  //       .send({
-  //         recordId: 1,
-  //       })
-  //       .expect(201)
-  //   }).timeout(6000)
-  // })
+  describe('DELETE /deleteRecord', () => {
+    it('should delete the record with the id from the inventory', async () => {
+      const addRes = await request('localhost:5001').post('/crud-firebase-1166f/us-central1/addRecord')
+        .type('application/json')
+        .send({
+          "itemName": "delete test item",
+          "itemDesc": "delete this is an item meant for testing",
+          "itemPrice": "5.00",
+          "itemQty": 10,
+          "inStock": true,
+          "category": "delete test category",
+        })
+        const { body } = addRes
+        const {data} = body
+        const recordId = data.id
+      const res = await request('localhost:5001').put('/crud-firebase-1166f/us-central1/deleteRecord')
+        .type('application/json')
+        .send({
+          id: recordId,
+        })
+        .expect(200)
+    }).timeout(12000)
+  })
